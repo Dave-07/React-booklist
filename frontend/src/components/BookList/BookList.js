2e5e7c45cb295d1966cd43407078892a4c6aece6
~D.t <165255878+Dave-07@users.noreply.github.com>
2025-10-10 20:03:52 +0200

    initialize Redux, process books

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,26 @@
 import './BookList.css'
+import { useSelector } from 'react-redux';
 
 const BookList = () => {
+    const books = useSelector(state => state.books);
+    const msg = books.length > 0
+        ? <ul>
+            {books.map((book, i) => {
+                return (
+                    <li key={i}>
+                        <div className='book-info'>
+                            {++i}. {book.title} by <strong>{book.author}</strong>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+        : <p>No book available</p>;
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
+            {msg}
         </div>
     )
 }
